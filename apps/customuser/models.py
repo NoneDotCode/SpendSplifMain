020fa282eb951bfb111505899c2219a40b2bce25
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-08-03 18:21:32 +0300

    edited: CustomUser model

--- apps/customuser/models.py
+++ apps/customuser/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from apps.customuser.constants import Language
+
+
+class CustomUser(AbstractUser):
+    """
+    Model class representing a custom user.
+    """
+
+    language = models.CharField(max_length=20, choices=Language.choices, default=Language.ENGLISH)
+    tag = models.PositiveIntegerField(null=True, blank=True, validators=[MaxValueValidator(9999)])
