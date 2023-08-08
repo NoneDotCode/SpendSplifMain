09012956be144128329bc48f62892b650cfae3cb
Bohdan <sqwfunny@gmail.com>
2023-08-08 16:23:01 +0200

    added new view "EditSpace"

--- apps/space/urls.py
+++ apps/space/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('create_space/', CreateSpace.as_view(), name='create_space'),
     path('my_spaces/', AllSpaces.as_view(), name='my_spaces'),
+    path('edit_space/<int:pk>/', EditSpace.as_view(), name='edit_space')
 ]
--- apps/space/views.py
+++ apps/space/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.reverse import reverse_lazy
 
 from .serializers import SpaceSerializer
@@ -16,3 +17,29 @@ class AllSpaces(generics.ListAPIView):
 
     def get_queryset(self):
         return Space.objects.filter(owner_id=self.request.user.id)
+
+
+# class EditSpace(ModelViewSet):
+#     def put(request):
+#         pk = request.data.get('pk')
+#         space = Space.objects.get(pk=pk)
+#         space.title = request.data.get('title')
+#         space.save()
+#         serializer = SpaceSerializer
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class EditSpace(generics.ListAPIView, generics.UpdateAPIView):
+    serializer_class = SpaceSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        return Space.objects.filter(pk=pk)
+
+    def put(self, request, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        space = Space.objects.get(pk=pk)
+        space.title = request.data.get('title')
+        space.save()
+        serializer = SpaceSerializer(space)
+        return Response(serializer.data, status=status.HTTP_200_OK)
