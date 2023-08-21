76e8fae7b17d8446a046407c766254f7a1bce522
Serhii Kyselov <tarfash.noko@gmail.com>
2023-08-21 15:35:36 +0200

    f

--- apps/space/views.py
+++ apps/space/views.py
@@ -19,27 +19,9 @@ class AllSpaces(generics.ListAPIView):
         return Space.objects.filter(owner_id=self.request.user.id)
 
 
-# class EditSpace(ModelViewSet):
-#     def put(request):
-#         pk = request.data.get('pk')
-#         space = Space.objects.get(pk=pk)
-#         space.title = request.data.get('title')
-#         space.save()
-#         serializer = SpaceSerializer
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
 class EditSpace(generics.ListAPIView, generics.UpdateAPIView):
     serializer_class = SpaceSerializer
 
     def get_queryset(self):
         pk = self.kwargs.get('pk')
         return Space.objects.filter(pk=pk)
-
-    def put(self, request, *args, **kwargs):
-        pk = self.kwargs.get('pk')
-        space = Space.objects.get(pk=pk)
-        space.title = request.data.get('title')
-        space.save()
-        serializer = SpaceSerializer(space)
-        return Response(serializer.data, status=status.HTTP_200_OK)
