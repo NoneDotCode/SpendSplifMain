3ee115d1873db3c3d011752a6bba8f02983656ca
Bohdan <sqwfunny@gmail.com>
2023-08-21 15:55:45 +0200

    edited: apps/space/views.py

--- apps/space/views.py
+++ apps/space/views.py
@@ -11,22 +11,10 @@ class CreateSpace(generics.CreateAPIView):
 
 
 class AllSpaces(generics.ListAPIView):
-    model = Space
-    login_url = reverse_lazy('token_obtain_pair')
     serializer_class = SpaceSerializer
 
     def get_queryset(self):
         return Space.objects.filter(owner_id=self.request.user.id)
-
-
-# class EditSpace(ModelViewSet):
-#     def put(request):
-#         pk = request.data.get('pk')
-#         space = Space.objects.get(pk=pk)
-#         space.title = request.data.get('title')
-#         space.save()
-#         serializer = SpaceSerializer
-#         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class EditSpace(generics.ListAPIView, generics.UpdateAPIView):
@@ -35,11 +23,3 @@ class EditSpace(generics.ListAPIView, generics.UpdateAPIView):
     def get_queryset(self):
         pk = self.kwargs.get('pk')
         return Space.objects.filter(pk=pk)
-
-    def put(self, request, *args, **kwargs):
-        pk = self.kwargs.get('pk')
-        space = Space.objects.get(pk=pk)
-        space.title = request.data.get('title')
-        space.save()
-        serializer = SpaceSerializer(space)
-        return Response(serializer.data, status=status.HTTP_200_OK)
