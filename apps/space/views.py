4af1b652c9e8ad3c9e0cc71e112caafff6b28a02
Serhii Kyselov <tarfash.noko@gmail.com>
2023-09-01 17:56:20 +0200

    added delete-view for Space, wrote permissions for space-edit and space-delete

--- apps/space/views.py
+++ apps/space/views.py
@@ -1,25 +1,34 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
-from rest_framework.reverse import reverse_lazy
+from rest_framework import generics
+
 
 from apps.space.models import Space
 from apps.space.serializers import SpaceSerializer
+from apps.space.permissions import IsOwner
 
 
 class CreateSpace(generics.CreateAPIView):
     serializer_class = SpaceSerializer
 
 
-class AllSpaces(generics.ListAPIView):
-    model = Space
+class ViewSpace(generics.ListAPIView):
     serializer_class = SpaceSerializer
 
     def get_queryset(self):
         return Space.objects.filter(owner_id=self.request.user.id)
 
 
-class EditSpace(generics.ListAPIView, generics.UpdateAPIView):
+class EditSpace(generics.RetrieveUpdateAPIView):
     serializer_class = SpaceSerializer
+    permission_classes = (IsOwner,)
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        return Space.objects.filter(pk=pk)
+
+
+class DeleteSpace(generics.RetrieveDestroyAPIView):
+    serializer_class = SpaceSerializer
+    permission_classes = (IsOwner,)
 
     def get_queryset(self):
         pk = self.kwargs.get("pk")
