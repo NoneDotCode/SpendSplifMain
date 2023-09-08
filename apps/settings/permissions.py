73ec8bdf5170731bfb652e941ace718e890906be
Bohdan <sqwfunny@gmail.com>
2023-09-08 09:46:41 +0200

    edited permissions

--- apps/settings/permissions.py
+++ apps/settings/permissions.py
@@ -5,18 +5,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     # пользователи могут редактировать только свои собственные объекты
 
     def has_object_permission(self, request, view, obj):
-        # разрешить чтение всем пользователям
-
-        # get и head запросы всегда разрешены
-        if request.method in permissions.SAFE_METHODS:
-            return True
 
         # разрешить запись только владельцу объекта
         if obj.pk == request.user.pk:
-            return True
-
-        # разрешить запись только администраторам
-        if request.user.is_superuser:
             return True
 
         return False
