a0f709b68689633518139e342264e95f690ca8b8
Serhii Kyselov <tarfash.noko@gmail.com>
2023-09-23 07:06:34 +0200

    Added incomes to accounts

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,9 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 from apps.account.models import Account
 from apps.account.serializers import AccountSerializer
-from apps.account.permissions import IsOwnerOfFatherSpace, IsInRightSpace, IsOwnerOfSpace
+from apps.account.permissions import IsOwnerOfFatherSpace, IsInRightSpace, IsOwnerOfSpace, IncomePermission
 
 from apps.space.models import Space
 
@@ -43,3 +44,25 @@ class DeleteAccount(generics.RetrieveDestroyAPIView):
     def get_queryset(self):
         pk = self.kwargs.get('pk')
         return Account.objects.filter(pk=pk)
+
+
+class IncomeView(generics.GenericAPIView):
+
+    def get_queryset(self):
+        return Account.objects.filter(pk=self.kwargs['pk'])
+
+    serializer_class = AccountSerializer
+    permission_classes = (IncomePermission,)
+
+    @staticmethod
+    def put(request, *args, **kwargs):
+        space_pk = kwargs.get('space_pk')
+        account_pk = kwargs.get('pk')
+        account = Account.objects.get(pk=account_pk)
+        amount = request.data.get('amount')
+        if amount is not None and amount > 0:
+            account.balance += amount
+            account.save()
+        else:
+            return Response({"error": "Please, fill out row amount, numbers bigger than 0."})
+        return Response({"success": "Income successfully completed."}, status=status.HTTP_200_OK)
