65ee7112c0470c12bf8e16dbe7a5eb0f11358c25
Serhii Kyselov <tarfash.noko@gmail.com>
2023-12-03 09:17:05 +0100

    edited all views for many members

--- apps/space/serializers.py
+++ apps/space/serializers.py
@@ -4,8 +4,11 @@ from apps.space.models import Space
 
 
 class SpaceSerializer(serializers.ModelSerializer):
-    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
     class Meta:
         model = Space
         fields = ("title", "members")
+
+
+class AddMemberToSpaceSerializer(serializers.Serializer):
+    user_pk = serializers.IntegerField(write_only=True)
+    fields = ("user_pk",)
--- apps/space/views.py
+++ apps/space/views.py
@@ -1,10 +1,11 @@
 from django.db import transaction
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-
+from apps.customuser.models import CustomUser
 from apps.space.models import Space, MemberPermissions
-from apps.space.serializers import SpaceSerializer
-from apps.space.permissions import IsSpaceOwner
+from apps.space.serializers import SpaceSerializer, AddMemberToSpaceSerializer
+from apps.space.permissions import IsSpaceOwner, IsMemberOfSpace, IsMemberOrOwnerOrCanAddMember
 
 
 class CreateSpace(generics.CreateAPIView):
@@ -23,26 +24,51 @@ class CreateSpace(generics.CreateAPIView):
             )
 
 
-class ViewSpace(generics.ListAPIView):
+class ListOfSpaces(generics.ListAPIView):
     serializer_class = SpaceSerializer
 
     def get_queryset(self):
-        return Space.objects.filter(owner_id=self.request.user.id)
+        return Space.objects.filter(members=self.request.user)
 
 
 class EditSpace(generics.RetrieveUpdateAPIView):
     serializer_class = SpaceSerializer
-    permission_classes = (IsSpaceOwner,)
+    permission_classes = (IsMemberOfSpace, IsSpaceOwner,)
 
     def get_queryset(self):
-        pk = self.kwargs.get("space_pk")
-        return Space.objects.filter(pk=pk)
+        return Space.objects.filter(pk=self.kwargs.get("pk"))
 
 
 class DeleteSpace(generics.RetrieveDestroyAPIView):
     serializer_class = SpaceSerializer
-    permission_classes = (IsSpaceOwner,)
+    permission_classes = (IsMemberOfSpace, IsSpaceOwner,)
 
     def get_queryset(self):
-        pk = self.kwargs.get("space_pk")
-        return Space.objects.filter(pk=pk)
+        return Space.objects.filter(pk=self.kwargs.get("pk"))
+
+
+class AddMemberToSpace(generics.GenericAPIView):
+    serializer_class = AddMemberToSpaceSerializer
+    permission_classes = (IsMemberOrOwnerOrCanAddMember,)
+
+    def put(self, request, *args, **kwargs):
+        space_pk = kwargs.get("pk")
+        user_pk = request.data.get("user_pk")
+
+        try:
+            space = Space.objects.get(pk=space_pk)
+            user = CustomUser.objects.get(pk=user_pk)
+        except (Space.DoesNotExist, CustomUser.DoesNotExist):
+            return Response({"error": "Space or user not found."},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        # Check if user is not yet member of the space.
+        if user in space.members.all():
+            return Response({"error": "User is member of the space already."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Add user to the space.
+        space.members.add(user)
+
+        # Return success answer
+        return Response({"success": "User successfully added to the space."}, status=status.HTTP_200_OK)
