4ee6ad7023c0ec4bc6cc8e3f57a4245b0ea8750c
Serhii Kyselov <tarfash.noko@gmail.com>
2024-02-03 11:25:00 +0100

    edited all permissions

--- backend/apps/space/permissions.py
+++ backend/apps/space/permissions.py
@@ -1,32 +1,6 @@
 from rest_framework.permissions import BasePermission
 
-from apps.customuser.models import CustomUser
 from backend.apps.space.models import Space
-
-
-class IsMemberAndOwnerOrCanRemoveMember(BasePermission):
-    def has_permission(self, request, view):
-        space_pk = view.kwargs.get("pk")
-        user_pk = request.data.get("user_pk")
-
-        try:
-            space = Space.objects.get(pk=space_pk)
-            CustomUser.objects.get(pk=user_pk)
-        except (Space.DoesNotExist, CustomUser.DoesNotExist):
-            return False
-
-        if request.user not in space.members.all():
-            return False
-
-        # Check if the user is the owner of the space
-        if space.memberpermissions_set.filter(member=request.user, owner=True).exists():
-            return True
-
-        # Check if the user has permission to remove members from the space
-        if not space.memberpermissions_set.filter(member=request.user, remove_users=True).exists():
-            return False
-
-        return True
 
 
 class IsSpaceMember(BasePermission):
@@ -40,6 +14,8 @@ class IsSpaceMember(BasePermission):
 
         if request.user not in space.members.all():
             return False
+
+        return True
 
 
 class IsSpaceOwner(BasePermission):
@@ -59,6 +35,28 @@ class CanAddMembers(BasePermission):
         space = Space.objects.get(pk=view.kwargs.get("pk"))
 
         if not space.memberpermissions_set.filter(member=request.user, add_members=True).exists():
+            return False
+
+        return True
+
+
+class CanRemoveMembers(BasePermission):
+
+    def has_permission(self, request, view):
+        space = Space.objects.get(pk=view.kwargs.get("pk"))
+
+        if not space.memberpermissions_set.filter(member=request.user, remove_members=True).exists():
+            return False
+
+        return True
+
+
+class CanEditMembers(BasePermission):
+
+    def has_permission(self, request, view):
+        space = Space.objects.get(pk=view.kwargs.get("pk"))
+
+        if not space.memberpermissions_set.filter(member=request.user, edit_members=True).exists():
             return False
 
         return True
