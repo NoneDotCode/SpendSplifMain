19e0cc81e6ca4de03cfbe69bc039f08e6e214d8c
Dima <conadosss@gmai.com>
2024-02-20 20:00:13 +0200

    delete note in views.py

--- apps/api_stocks/views.py
+++ apps/api_stocks/views.py
@@ -5,34 +5,7 @@ import requests
 from apps.api_stocks.models import Stock
 
 
-# class UpdateStocksAPIView_group1(APIView):
-#     api_key = '296b89a58663457d9dcd754263b549bf'
-#     symbols_to_check = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'ORCL', 'TSLA', 'BABA', 'JNJ']
 
-#     def get(self, request, *args, **kwargs):
-#         response_data = {}
-#         for symbol in self.symbols_to_check:
-#             time_series_url = f'https://api.twelvedata.com/time_series?symbol={symbol}&interval=1min&apikey={self.api_key}'
-#             time_series_response = requests.get(time_series_url)
-
-#             try:
-#                 data = time_series_response.json()['values']
-#                 if data:
-#                     current_price = data[-1]['close']
-#                     response_data[symbol] = current_price
-#                     stock, created = Stock.objects.get_or_create(symbol=symbol, defaults={'price': current_price})
-
-#                     if not created:
-#                         stock.price = current_price
-#                         stock.save()
-#                 else:
-#                     response_data[symbol] = "No data available"
-#                     print(f"No data available for {symbol}")
-#             except KeyError as e:
-#                 response_data[symbol] = f"Error retrieving data: {e}"
-#                 print(f"Error retrieving data for {symbol}: {e}")
-
-#         return Response(response_data, status=status.HTTP_200_OK)
 
 class UpdateStocksAPIView_group1(APIView):
     api_key = '296b89a58663457d9dcd754263b549bf'
