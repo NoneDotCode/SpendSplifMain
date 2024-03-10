c889f7d6fec8519ee632db34114b8b5acf3e3ba1
Serhii Kyselov <tarfash.noko@gmail.com>
2024-03-10 13:00:29 +0100

    changed nothing

--- backend/apps/history/models.py
+++ backend/apps/history/models.py
@@ -12,7 +12,7 @@ class HistoryExpense(models.Model):
     currency = models.CharField(max_length=3, choices=Currency.choices, default=Currency.UNITED_STATES_DOLLAR)
     comment = models.CharField(max_length=300)
     from_acc = models.CharField(max_length=24)
-    to_cat = models.CharField(max_length=24, null=True)
+    to_cat = models.CharField(max_length=24, blank=True)
     father_space = models.ForeignKey(Space, verbose_name='father_space', on_delete=models.CASCADE)
     created = models.DateTimeField(default=datetime.now)
 
@@ -20,7 +20,7 @@ class HistoryExpense(models.Model):
 class HistoryIncome(models.Model):
     amount = models.DecimalField(max_digits=20, decimal_places=2)
     currency = models.CharField(max_length=4, choices=Currency.choices, default=Currency.UNITED_STATES_DOLLAR)
-    comment = models.CharField(max_length=300, null=True, blank=True)
+    comment = models.CharField(max_length=300, blank=True)
     account = models.CharField(max_length=24)
     father_space = models.ForeignKey(Space, verbose_name='father_space', on_delete=models.CASCADE)
     created = models.DateTimeField(default=datetime.now)
