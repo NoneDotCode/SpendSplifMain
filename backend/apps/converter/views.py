363b7b73fb61efebb1bc2344eebd157ce7d7fa38
Serhii Kyselov <tarfash.noko@gmail.com>
2024-03-31 12:19:12 +0200

    converter allow any

--- backend/apps/converter/views.py
+++ backend/apps/converter/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from backend.apps.converter.utils import convert_currencies
 
 
 class ConvertCurrencyView(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
     def get(self, request, **kwargs):
         from_currency = self.request.data.get('from_currency')
         to_currency = self.request.data.get('to_currency')
