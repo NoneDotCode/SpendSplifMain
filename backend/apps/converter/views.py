718def29fd8f9558d909e1f0ce29ef2d5f6c0b34
Bohdan <sqwfunny@gmail.com>
2024-04-09 15:34:16 +0200

    added Wxpo urls to settings

--- backend/apps/converter/views.py
+++ backend/apps/converter/views.py
@@ -1,15 +1,18 @@
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 from backend.apps.converter.utils import convert_currencies
 
 
 class ConvertCurrencyView(generics.RetrieveAPIView):
-    def get(self, request, **kwargs):
+    permission_classes = [AllowAny,]
+
+    def post(self, request, **kwargs):
         from_currency = self.request.data.get('from_currency')
         to_currency = self.request.data.get('to_currency')
         amount = self.request.data.get('amount')
 
         converted_amount = convert_currencies(from_currency=from_currency, to_currency=to_currency, amount=amount)
 
-        return Response({'Converted successfully:': f"{amount}{from_currency} = {converted_amount}{to_currency}"})
+        return Response({'Converted successfully:': f"{amount} {from_currency} = {converted_amount} {to_currency}"})
