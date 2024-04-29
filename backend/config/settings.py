0fbeeca7874c0d66ee3709a34fd19da3a1ba1104
Serhii Kyselov <tarfash.noko@gmail.com>
2024-04-29 22:29:45 +0200

    asdg"

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -203,6 +203,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:8000",
+    "http://34.140.246.141:5473/",
     "exp://bw-wbvi.anonymous.8081.exp.direct",
     "https://ee76-88-103-194-16.ngrok-free.app",
 ]
@@ -213,6 +214,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "http://34.140.246.141:5473/",
     "exp://bw-wbvi.anonymous.8081.exp.direct",
     "https://ee76-88-103-194-16.ngrok-free.app",
 ]
@@ -221,11 +223,13 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = "Lax"
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
+    "http://34.140.246.141:5473/",
     "http://127.0.0.1:8000",
     ]
 SESSION_COOKIE_SAMESITE = "Lax"
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:5173",
+    "http://34.140.246.141:5473/",
     "http://127.0.0.1:8000",
     "exp://bw-wbvi.anonymous.8081.exp.direct",
     "https://ee76-88-103-194-16.ngrok-free.app",
