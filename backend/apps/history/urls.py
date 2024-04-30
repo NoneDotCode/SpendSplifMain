361860d330fec9076d1b481eca15ae49070264b9
Maks328 <96689357+Maksim325@users.noreply.github.com>
2024-04-30 18:37:59 +0300

    created url for income_edit view

--- backend/apps/history/urls.py
+++ backend/apps/history/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from backend.apps.history.views import HistoryView, DailyIncomeView, AutoDataView, HistoryExpenseEdit
+from backend.apps.history.views import HistoryView, DailyIncomeView, AutoDataView, HistoryExpenseEdit, HistoryIncomeEdit
 
 urlpatterns = [
     path('my_history/', HistoryView.as_view(), name='check_space_history'),
     path('daily_income/', DailyIncomeView.as_view(), name='daily_income'),
     path('auto_data/', AutoDataView.as_view(), name='auto_data'),
     path("edit_expense/<int:pk>/", HistoryExpenseEdit.as_view(), name="edit_expense"),
+    path("edit_income/<int:pk>/", HistoryIncomeEdit.as_view(), name="edit_income"),
 ]
