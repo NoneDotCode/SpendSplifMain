5c641032983caab38bdf1f989203d4ea6646e525
Serhii Kyselov <tarfash.noko@gmail.com>
2024-05-04 16:40:50 +0200

    added logout

--- backend/apps/customuser/urls.py
+++ backend/apps/customuser/urls.py
@@ -6,6 +6,6 @@ from backend.apps.customuser.views import *
 urlpatterns = [
     path("register/", CustomUserRegistrationView.as_view(), name="register"),
     path("verify_email/", ConfirmRegistrationView.as_view(), name="verifyCode"),
-    # path("me/password_reseе/send_code/", SendResetCodeView.as_view(), name="resetSendCode"),
+    path("me/logout/", LogoutView.as_view(), name="logout"),
     # path("me/password_reset/reset/", ResetPasswordView.as_view(), name="resetPassword"),
 ]
