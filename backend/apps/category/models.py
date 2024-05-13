206c3bfea1d31f4451e0ee3ea5eed5fff23dba49
Serhii Kyselov <tarfash.noko@gmail.com>
2024-05-13 07:01:57 +0200

    added order to categories

--- backend/apps/category/models.py
+++ backend/apps/category/models.py
@@ -8,11 +8,11 @@ from backend.apps.space.models import Space
 
 class Category(models.Model):
     title = models.CharField(max_length=24)
-    icon = models.FilePathField()
     spent = models.DecimalField(max_digits=20, decimal_places=2)
     limit = models.DecimalField(max_digits=20, decimal_places=2, null=True)
     color = ColorField(format="hex")
     icon = models.CharField(choices=Icons.choices)
+    order = models.PositiveSmallIntegerField(blank=False, null=False, default=0)
     father_space = models.ForeignKey(Space, verbose_name='father_space', on_delete=models.CASCADE)
 
     def __str__(self):
--- backend/apps/category/serializers.py
+++ backend/apps/category/serializers.py
@@ -7,7 +7,8 @@ from backend.apps.space.models import Space
 
 class CategorySerializer(serializers.ModelSerializer):
     father_space = serializers.PrimaryKeyRelatedField(queryset=Space.objects.all(), required=False)
+    order = serializers.IntegerField(required=False)
 
     class Meta:
         model = Category
-        fields = ('title', 'spent', 'limit', 'color', 'icon', 'father_space')
+        fields = ('title', 'spent', 'limit', 'color', 'icon', 'order', 'father_space')
