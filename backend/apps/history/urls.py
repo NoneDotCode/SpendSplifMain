ba3883cc8e768f207e5d118983dd9ff18829fc8a
Max_G <garvatmaksim@gmail.com>
2024-05-30 21:03:11 +0300

    added urls for editing transfer

--- backend/apps/history/urls.py
+++ backend/apps/history/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from backend.apps.history.views import HistoryView, ExpenseAutoDataView, IncomeAutoDataView, \
     TransferAutoDataView, CategoryStatisticView, IncomeStatisticView, ExpensesStatisticView, StatisticView, \
-    GoalTransferStatisticView, GeneralView, StatisticSimulation
+    GoalTransferStatisticView, GeneralView, StatisticSimulation, HistoryTransferEdit
 
 urlpatterns = [
     path('my_history/', HistoryView.as_view(), name='check_space_history'),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('auto_transfer/', TransferAutoDataView.as_view(), name='transfer'),
     path('statistic/', StatisticView.as_view(), name='stat'),
     path('statistic_simulation/', StatisticSimulation.as_view(), name='stat'),
+    path('edit_transfer/<int:pk>/', HistoryTransferEdit.as_view(), name='edit_history_trasfer'),
 ]
