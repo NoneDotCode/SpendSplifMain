90d73716b1e0409527c02da318c467dd858de2d9
Max_G <garvatmaksim@gmail.com>
2024-06-01 18:12:06 +0300

    added multiplier to categories

--- backend/apps/category/serializers.py
+++ backend/apps/category/serializers.py
@@ -4,12 +4,14 @@ from backend.apps.category.models import Category
 
 from backend.apps.space.models import Space
 
+from backend.apps.converter.utils import convert_number_to_letter  
 
 class CategorySerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     father_space = serializers.PrimaryKeyRelatedField(queryset=Space.objects.all(), required=False)
     order = serializers.IntegerField(required=False)
     spent = serializers.IntegerField(required=False)
+    limit = serializers.IntegerField(required=False)
     icon = serializers.CharField(required=False)
     spent_percentage = serializers.SerializerMethodField()
 
@@ -26,4 +28,10 @@ class CategorySerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['spent_percentage'] = self.get_spent_percentage(instance)
+        
+        if representation['spent']:
+            representation['spent'] = convert_number_to_letter(representation['spent'])
+        if representation['limit']:
+            representation['limit'] = convert_number_to_letter(representation['limit'])
+        
         return representation
