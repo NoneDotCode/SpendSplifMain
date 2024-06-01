b3f2a3a77ca5dbaa71e94c46e147b3fe6948e939
Max_G <garvatmaksim@gmail.com>
2024-06-01 18:08:32 +0300

    added a function to convert a number to a factor

--- backend/apps/converter/utils.py
+++ backend/apps/converter/utils.py
@@ -7,3 +7,21 @@ def convert_currencies(*, from_currency, to_currency, amount: float):
     euro_amount = decimal.Decimal(amount) / Currency.objects.get(iso_code=from_currency).euro
     converted_amount = round(euro_amount * Currency.objects.get(iso_code=to_currency).euro, 2)
     return converted_amount
+
+def convert_number_to_letter(number: float) -> str:
+    """Convert a number to string with multiplier 
+    for example from 1700 to 1.7k"""
+
+    suffixes = {1000000000: 'b', 1000000: 'm', 1000: 'k'}
+    if number:
+
+        for key in sorted(suffixes.keys(), reverse=True):
+
+            if number >= key:
+                if int(number) == number:
+                    return f"{int(number / key)}{suffixes[key]}"
+                
+                return f"{number / key:.2f}{suffixes[key]}"
+            
+        return str(number)
+    return '0'
