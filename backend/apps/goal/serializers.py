622cf10d4de0f4a04bc3d7aa57d7fe0c6f1b2e67
Serhii Kyselov <tarfash.noko@gmail.com>
2024-06-04 16:13:58 +0200

    added in goal serializer format_collected

--- backend/apps/goal/serializers.py
+++ backend/apps/goal/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
-
 from backend.apps.goal.models import Goal
+from backend.apps.converter.utils import convert_number_to_letter
 
 
 class GoalSerializer(serializers.ModelSerializer):
+    format_collected = serializers.SerializerMethodField()
+
     class Meta:
         model = Goal
-        fields = ('title', 'goal', 'collected', 'father_space', 'created')
+        fields = ('title', 'goal', 'collected', 'father_space', 'created', 'format_collected')
+
+    @staticmethod
+    def get_format_collected(obj):
+        return convert_number_to_letter(obj.collected)
