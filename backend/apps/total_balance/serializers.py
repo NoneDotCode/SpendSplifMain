ed2fc6994228d9a7b472454e0d842b4723cfaf27
Serhii Kyselov <tarfash.noko@gmail.com>
2024-06-18 21:01:21 +0200

    formatted balance in total_balance serializer

--- backend/apps/total_balance/serializers.py
+++ backend/apps/total_balance/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
 from backend.apps.total_balance.models import TotalBalance
+from backend.apps.converter.utils import convert_number_to_letter
 
 
 class TotalBalanceSerializer (serializers.ModelSerializer):
-    balance = serializers.DecimalField(required=False, max_digits=20, decimal_places=2)
+    balance = serializers.SerializerMethodField()
     currency = serializers.SerializerMethodField()
 
     class Meta:
@@ -14,3 +15,7 @@ class TotalBalanceSerializer (serializers.ModelSerializer):
     @staticmethod
     def get_currency(obj):
         return obj.father_space.currency if obj.father_space else None
+
+    @staticmethod
+    def get_balance(obj):
+        return convert_number_to_letter(float(obj.balance))
