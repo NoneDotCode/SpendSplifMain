bab5f2f7c382d2539d3efc3d49ca883bb7bce29c
Serhii Kyselov <tarfash.noko@gmail.com>
2024-07-04 17:15:38 +0200

    Added formatted_balance to TotalBalanceSerializer

--- backend/apps/total_balance/serializers.py
+++ backend/apps/total_balance/serializers.py
@@ -5,17 +5,17 @@ from backend.apps.converter.utils import convert_number_to_letter
 
 
 class TotalBalanceSerializer (serializers.ModelSerializer):
-    balance = serializers.SerializerMethodField()
     currency = serializers.SerializerMethodField()
+    formatted_balance = serializers.SerializerMethodField()
 
     class Meta:
         model = TotalBalance
-        fields = ('balance', 'currency')
+        fields = ('balance', 'formatted_balance', 'currency')
 
     @staticmethod
     def get_currency(obj):
         return obj.father_space.currency if obj.father_space else None
 
     @staticmethod
-    def get_balance(obj):
+    def get_formatted_balance(obj):
         return convert_number_to_letter(float(obj.balance))
