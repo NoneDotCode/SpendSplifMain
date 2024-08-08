18ffca27e3d504888cb0d2349341badf385e8112
Serhii Kyselov <tarfash.noko@gmail.com>
2024-08-08 19:56:31 +0200

    Added to middleware.py host printing

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "backend.config.middleware.CheckHostMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
