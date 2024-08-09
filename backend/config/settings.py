549b1e40ddabf44c5a51ef0658f68f8245611234
Serhii Kyselov <tarfash.noko@gmail.com>
2024-08-09 12:16:07 +0200

    Edited database configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "spendsplif",
         "USER": "postgres",
-        "PASSWORD": "spendsplifchek",
+        "PASSWORD": "gc9!V-APp!69vL8",
         "HOST": "localhost",
         "PORT": 5432,
     }
