3bee383ba4d5626812db1f37671959cde53b694d
Serhii Kyselov <tarfash.noko@gmail.com>
2024-08-09 12:30:15 +0200

    Edited wsgi.py config

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.config.settings")
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
