4d0df38a29215294d7a0cbe9ac141942e1a03603
Serhii Kyselov <tarfash.noko@gmail.com>
2024-08-14 12:22:31 +0200

    .

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "backend.config.middleware.IPAndHeaderCheckMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
