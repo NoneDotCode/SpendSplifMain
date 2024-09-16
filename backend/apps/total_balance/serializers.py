7cdbf8fdeb7d5692e24f9c48934ae3b86ffa2297
Max_g <96689357+Maksim325@users.noreply.github.com>
2024-09-16 19:36:06 +0300

    Update serializers.py

--- backend/apps/total_balance/serializers.py
+++ backend/apps/total_balance/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
-
 from backend.apps.total_balance.models import TotalBalance
 from backend.apps.converter.utils import convert_number_to_letter
 
-
-class TotalBalanceSerializer (serializers.ModelSerializer):
+class TotalBalanceSerializer(serializers.ModelSerializer):
     total_expenses_this_month = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
     total_income_this_month = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
     currency = serializers.SerializerMethodField()
@@ -12,7 +10,7 @@ class TotalBalanceSerializer (serializers.ModelSerializer):
 
     class Meta:
         model = TotalBalance
-        fields = ('balance', 'formatted_balance', 'currency')
+        fields = ('balance', 'formatted_balance', 'currency', 'total_expenses_this_month', 'total_income_this_month')
 
     @staticmethod
     def get_currency(obj):
@@ -21,4 +19,3 @@ class TotalBalanceSerializer (serializers.ModelSerializer):
     @staticmethod
     def get_formatted_balance(obj):
         return convert_number_to_letter(float(obj.balance))
-    
