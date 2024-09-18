d031ee164d83e8783f03ea75cc46c3f8fcbf6de1
Serhii Kyselov <tarfash.noko@gmail.com>
2024-09-18 06:58:18 +0200

    Fixed total_balance

--- backend/apps/total_balance/serializers.py
+++ backend/apps/total_balance/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
+from django.utils import timezone
+from django.db.models import Sum
+
+from backend.apps.history.models import HistoryExpense, HistoryIncome
 from backend.apps.total_balance.models import TotalBalance
 from backend.apps.converter.utils import convert_number_to_letter
 
+
 class TotalBalanceSerializer(serializers.ModelSerializer):
-    total_expenses_this_month = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
-    total_income_this_month = serializers.DecimalField(max_digits=12, decimal_places=2, read_only=True)
     currency = serializers.SerializerMethodField()
     formatted_balance = serializers.SerializerMethodField()
+    total_income_this_month = serializers.SerializerMethodField()
+    total_expenses_this_month = serializers.SerializerMethodField()
 
     class Meta:
         model = TotalBalance
@@ -19,3 +24,27 @@ class TotalBalanceSerializer(serializers.ModelSerializer):
     @staticmethod
     def get_formatted_balance(obj):
         return convert_number_to_letter(float(obj.balance))
+
+    @staticmethod
+    def get_total_expenses_this_month(obj):
+        today = timezone.now()
+        start_of_month = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+        total_expenses = HistoryExpense.objects.filter(
+            father_space_id=obj.father_space.id,
+            created__gte=start_of_month
+        ).aggregate(total=Sum('amount_in_default_currency'))['total'] or 0
+
+        return total_expenses
+
+    @staticmethod
+    def get_total_income_this_month(obj):
+        today = timezone.now()
+        start_of_month = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+        total_income = HistoryIncome.objects.filter(
+            father_space_id=obj.father_space.id,
+            created__gte=start_of_month
+        ).aggregate(total=Sum('amount_in_default_currency'))['total'] or 0
+
+        return total_income
--- backend/apps/total_balance/views.py
+++ backend/apps/total_balance/views.py
@@ -15,32 +15,3 @@ class ViewTotalBalance(generics.ListAPIView):
 
     def get_queryset(self):
         return TotalBalance.objects.filter(father_space_id=self.kwargs["space_pk"])
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        
-        # Get the start of the current month
-        today = timezone.now()
-        start_of_month = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
-
-        # Calculate total expenses for this month
-        total_expenses = HistoryExpense.objects.filter(
-            father_space_id=self.kwargs["space_pk"],
-            created__gte=start_of_month
-        ).aggregate(total=Sum('amount_in_default_currency'))['total'] or 0
-
-        # Calculate total income for this month
-        total_income = HistoryIncome.objects.filter(
-            father_space_id=self.kwargs["space_pk"],
-            created__gte=start_of_month
-        ).aggregate(total=Sum('amount_in_default_currency'))['total'] or 0
-
-        serializer = self.get_serializer(queryset, many=True)
-        data = serializer.data
-
-        # Add the monthly totals to the response
-        for item in data:
-            item['total_expenses_this_month'] = total_expenses
-            item['total_income_this_month'] = total_income
-
-        return Response(data)
