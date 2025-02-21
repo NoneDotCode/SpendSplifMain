a28daae7029c419cf5c60dbef5cb10b74107f071
Bohdan <sqwfunny@gmail.com>
2025-02-21 19:50:44 +0100

    FinAPI test version v4

--- backend/apps/cards/urls.py
+++ backend/apps/cards/urls.py
@@ -4,12 +4,14 @@ from backend.apps.cards.views import (
     BankConnectionWebhook,
     BankTransactionsAndBalanceWebhook,
     BanksView,
-    UserSpaceView
+    UserSpaceView,
+    SpaceBankConnectionsView
 )
 
 urlpatterns = [
     path('bank/connection/', BankConnectionView.as_view(), name='bank-connection'),
     path('banks/', BanksView.as_view(), name='czech-banks'),
+    path('my_connections/', SpaceBankConnectionsView.as_view(), name='my-connections'),
     path('user-bank/', UserSpaceView.as_view(), name='user-space'),
     path('webhook/bank/connection/', BankConnectionWebhook.as_view(), name='bank-connection-webhook'),
     path('webhook/bank/transactions/', BankTransactionsAndBalanceWebhook.as_view(), name='bank-transactions-webhook'),
