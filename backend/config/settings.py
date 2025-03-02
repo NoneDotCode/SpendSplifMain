b1d7c52928fadfc9ef86b8a2532ef8369be33dc7
root <root@app-vm-03.europe-west4-b.c.spendsplif-000001.internal>
2025-03-02 13:57:19 +0000

    recreate migrations

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY', 'defaultsecret')
 def get_debug_setting():
     debug_str = os.environ.get('DEBUG', 'True')  
     return debug_str.lower() != 'false'  
