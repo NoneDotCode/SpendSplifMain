cd5b6a57f7bd0ea9158efcf3b6c201850879a15a
Bohdan <sqwfunny@gmail.com>
2025-05-09 18:46:37 +0200

    space backup fixed

--- backend/apps/space/tasks.py
+++ backend/apps/space/tasks.py
@@ -1,5 +1,7 @@
 from celery import shared_task
 from django.utils import timezone
+import json
+from decimal import Decimal
 
 from backend.apps.account.serializers import AccountSerializer
 from backend.apps.category.serializers import CategorySerializer
@@ -8,17 +10,35 @@ from backend.apps.total_balance.models import TotalBalance
 from backend.apps.total_balance.serializers import TotalBalanceSerializer
 
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super().default(obj)
+
+
 @shared_task
 def create_space_backup():
     spaces = Space.objects.all()
     for space in spaces:
         backup_date = timezone.now().date()
 
-        accounts = AccountSerializer(space.account_set.all(), many=True).data
-        categories = CategorySerializer(space.category_set.all(), many=True).data
+        accounts = json.loads(json.dumps(
+            AccountSerializer(space.account_set.all(), many=True).data,
+            cls=DecimalEncoder
+        ))
+        categories = json.loads(json.dumps(
+            CategorySerializer(space.category_set.all(), many=True).data,
+            cls=DecimalEncoder
+        ))
 
         total_balance = TotalBalance.objects.filter(father_space=space).first()
-        total_balance_data = TotalBalanceSerializer(total_balance).data if total_balance else None
+        total_balance_data = None
+        if total_balance:
+            total_balance_data = json.loads(json.dumps(
+                TotalBalanceSerializer(total_balance).data,
+                cls=DecimalEncoder
+            ))
 
         SpaceBackup.objects.create(
             father_space=space,
